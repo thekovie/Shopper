01cf4fe4529f60b9fc0f4f6d58797ab02a7ee4ec
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2024-10-12 19:36:14 +0800

    feat: add sample dynamic search results function

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, ScrollView } from "react-native";
 import { InputSearch } from "@/components/ui/input-search";
 import ListShoppingItem from '@/components/list/ListShoppingItem';
@@ -5,6 +6,8 @@ import { Text } from "@/components/ui/text";
 import { ListShoppingItemProps, RecentFindsProps } from "@/constants/types";
 
 export default function Tab() {
+
+  const [searchInput, setSearchInput] = useState('');
 
   const sampleItemsData: ListShoppingItemProps[] = [
     {
@@ -34,7 +37,45 @@ export default function Tab() {
         itemPriority: 'Priority',
         itemPlatform: 'Shopee',
         itemCategory: 'Mobiles & GadgetsAWDAWDAWDAWDAWD ABWVDHGAWDVGHAW AHGJWDGHAWDGHAWD  HGAWDGHAWDGH'
-    }
+    },
+  ];
+
+  const samepleResultsData: ListShoppingItemProps[] = [
+    {
+        itemName: 'iPhone 15 Pro Max Case Fully Loaded',
+        itemPrice: 700,
+        itemPriority: 'Priority',
+        itemPlatform: 'Shopee',
+        itemCategory: 'Mobiles & Gadgets'
+    },
+    {
+        itemName: 'iPhone 15 Pro Max Case Fully Loaded',
+        itemPrice: 700,
+        itemPriority: 'Priority',
+        itemPlatform: 'Shopee',
+        itemCategory: 'Mobiles & Gadgets'
+    },
+    {
+        itemName: 'Hotdog 15 Pro Max Case Fully Loaded',
+        itemPrice: 700,
+        itemPriority: 'Priority',
+        itemPlatform: 'Shopee',
+        itemCategory: 'Mobiles & Gadgets'
+    },
+    {
+        itemName: 'Strawberry 15 Pro Max Case Fully Loaded',
+        itemPrice: 700,
+        itemPriority: 'Priority',
+        itemPlatform: 'Shopee',
+        itemCategory: 'Mobiles & GadgetsAWDAWDAWDAWDAWD ABWVDHGAWDVGHAW AHGJWDGHAWDGHAWD  HGAWDGHAWDGH'
+    },
+    {
+      itemName: 'Chiken NUggets',
+      itemPrice: 700,
+      itemPriority: 'Priority',
+      itemPlatform: 'Shopee',
+      itemCategory: 'Mobiles & Gadgets'
+    },
   ];
 
   return (
@@ -42,18 +83,48 @@ export default function Tab() {
     <InputSearch
       placeholder="Search for your shopping item"
       className="text-lonestar-500 border-[#e4e4e7] mb-[10]"
-    />
-    <Text className="text-lonestar-600 text-sm mb-[10]" fontVariant="SemiBold">
-      Recent Finds
-    </Text>
+      onChangeText={(text) => {
+        setSearchInput(text);
+      }}
 
+    />
+    {searchInput.length < 1 ? (
+      <Text className="text-lonestar-600 text-sm mb-[10]" fontVariant="SemiBold">
+        Recent Finds 
+      </Text>
+    ): (
+      <Text className="text-lonestar-600 text-sm mb-[10]" fontVariant="SemiBold">
+        Results for {searchInput}
+      </Text>
+    )}
+    
+
+    {searchInput.length < 1 &&  
     <View className='flex flex-col overflow-hidden'>
       {sampleItemsData.map(({itemName, itemPrice, itemPriority, itemPlatform, itemCategory}, index) => (
         <View key={index} className='mb-[20]'>
             <ListShoppingItem itemName={itemName} itemPrice={itemPrice} itemPriority={itemPriority} itemPlatform={itemPlatform} itemCategory={itemCategory} />
         </View>
       ))}    
-    </View>
+    </View>}
+
+    {searchInput.length > 0 && samepleResultsData
+      .filter(({ itemName }) =>
+        itemName.toLowerCase().includes(searchInput.toLowerCase())
+      )
+      .map(({ itemName, itemPrice, itemPriority, itemPlatform, itemCategory }, index) => (
+        <View key={index} className='mb-[20]'>
+          <ListShoppingItem
+            itemName={itemName}
+            itemPrice={itemPrice}
+            itemPriority={itemPriority}
+            itemPlatform={itemPlatform}
+            itemCategory={itemCategory}
+          />
+        </View>
+    ))}
+
+
 
    </View>
   );
