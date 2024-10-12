24e3d0ba3ed06a57950a0263a689268a6e0eaffc
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2024-10-12 17:47:40 +0800

    refactor: remove unused imports

--- components/add-shopping-item/CancelChanges.tsx
+++ components/add-shopping-item/CancelChanges.tsx
@@ -3,13 +3,9 @@ import { TouchableOpacity, View } from 'react-native';
 import { router } from 'expo-router';
 import {
     AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
     AlertDialogContent,
-    AlertDialogDescription,
     AlertDialogFooter,
     AlertDialogHeader,
-    AlertDialogTitle,
     AlertDialogTrigger,
   } from '@/components/ui/alert-dialog';
   import { Button } from '@/components/ui/button';
