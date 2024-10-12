73060a8debd05cf6277d08272d22d25f6f13092d
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2024-10-12 23:44:37 +0800

    refactor: fix inconsistent search input border

--- components/ui/input-search.tsx
+++ components/ui/input-search.tsx
@@ -12,11 +12,11 @@ const InputSearch = React.forwardRef<
   const scheme = useColorScheme(); // Detect system theme
 
   return (
-    <View className="flex flex-row">
+    <View className="flex flex-row overflow-hidden">
         <TextInput
           ref={ref}
           className={cn(
-            "native:h-12 native:text-lg native:leading-[1.25] h-10 rounded-t-md rounded-b-md rounded-left-md rounded-r-none border-t border-b border-l border-input bg-background px-3 pr-4 text-base text-foreground file:border-0 file:bg-transparent file:font-medium placeholder:text-muted-foreground web:flex web:w-full web:py-2 web:ring-offset-background web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 lg:text-sm",
+            "native:h-12 native:text-lg native:leading-[1.25] h-10 rounded-t-md rounded-b-md rounded-left-md rounded-tr-none rounded-br-none border-t border-b border-l border-input bg-background px-3 pr-4 text-base text-foreground file:border-0 file:bg-transparent file:font-medium placeholder:text-muted-foreground web:flex web:w-full web:py-2 web:ring-offset-background web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 lg:text-sm",
             props.editable === false && "opacity-50 web:cursor-not-allowed",
             className
           )}
@@ -29,7 +29,7 @@ const InputSearch = React.forwardRef<
             },
           ]}
         />
-        <TouchableOpacity className="flex items-center justify-center p-[8] native:h-12 h-10 border border-[#D3D3D3] rounded-l-none rounded-t-md rounded-b-md rounded-r-md ">
+        <TouchableOpacity className="flex items-center justify-center p-[8] native:h-12 h-10 border border-[#D3D3D3] rounded-md rounded-l-none ">
             <Search size={20} color="#A2A3A3" className="" style={{ transform: [{ rotate: '360deg' }] }}/>
         </TouchableOpacity>
     </View>
