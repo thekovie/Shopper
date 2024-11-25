bf0b100f80de7074b303f9e38c4b4f55d6b658de
John Kovie Nino <thekovie@gmail.com>
2024-11-25 15:43:35 +0800

    feat: add router to View All text

--- components/homepage/RecentFinds.tsx
+++ components/homepage/RecentFinds.tsx
@@ -1,48 +1,62 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text } from '@/components/ui/text'
-import ListShoppingItem from '@/components/list/ListShoppingItem';
-import { ExtendedShoppingItemInsert } from '@/constants/types';
+import React from "react";
+import { Pressable, View } from "react-native";
+import { Text } from "@/components/ui/text";
+import ListShoppingItem from "@/components/list/ListShoppingItem";
+import { ExtendedShoppingItemInsert } from "@/constants/types";
+import { router } from "expo-router";
 
-
-interface Props{
-    shoppingItems: ExtendedShoppingItemInsert[] | null;
+interface Props {
+  shoppingItems: ExtendedShoppingItemInsert[] | null;
 }
 
 function RecentFinds({ shoppingItems }: Props) {
-    
-    return (
-        <View className="flex flex-col bg-lonestar-50 py-[20] px-[15] rounded-xl mb-[24]">
-            <View className='flex flex-row justify-between w-full mb-[8]'>
-                <View className='flex flex-col'>
-                    <Text className='text-lonestar-600 text-base' fontVariant='SemiBold'>Recent Finds</Text>
-                    <Text className='text-lonestar-900 text-xs'>Check out your recent interests below!</Text>
-                </View>
-                <Text className='text-lonestar-600 text-xs'>View All</Text>
-            </View>
+  const userId = shoppingItems?.[0].user_id;
 
-            <View className='flex flex-col overflow-hidden'>
-                
-            {shoppingItems?.map((shoppingItem, index) => (
-              <View key={index} className='mb-[20]'>
-                  <ListShoppingItem 
-                      id={shoppingItem.id}
-                      product_title={shoppingItem.product_title} 
-                      price={shoppingItem.price!} 
-                      priority={shoppingItem.priority} 
-                      shopping_platform={shoppingItem.shopping_platform} 
-                      category_id={shoppingItem.category_id} 
-                      notes={shoppingItem.notes} 
-                      is_purchased={shoppingItem.is_purchased}
-                      user_id={shoppingItem.user_id}
-                      category_name={shoppingItem.category_name}
-                  />
-              </View>
-            ))}    
-            </View>
-
+  return (
+    <View className="mb-[24] flex flex-col rounded-xl bg-lonestar-50 px-[15] py-[20]">
+      <View className="mb-[8] flex w-full flex-row justify-between">
+        <View className="flex flex-col">
+          <Text className="text-base text-lonestar-600" fontVariant="SemiBold">
+            Recent Finds
+          </Text>
+          <Text className="text-xs text-lonestar-900">
+            Check out your recent interests below!
+          </Text>
         </View>
-    );
+        <Pressable
+          onPress={() => {
+            router.push({
+              pathname: "/(shopping-list-menu)/recent-finds",
+              params: {
+                user_id: userId,
+              },
+            });
+          }}
+        >
+          <Text className="text-xs text-lonestar-600">View All</Text>
+        </Pressable>
+      </View>
+
+      <View className="flex flex-col overflow-hidden">
+        {shoppingItems?.map((shoppingItem, index) => (
+          <View key={index} className="mb-[20]">
+            <ListShoppingItem
+              id={shoppingItem.id}
+              product_title={shoppingItem.product_title}
+              price={shoppingItem.price!}
+              priority={shoppingItem.priority}
+              shopping_platform={shoppingItem.shopping_platform}
+              category_id={shoppingItem.category_id}
+              notes={shoppingItem.notes}
+              is_purchased={shoppingItem.is_purchased}
+              user_id={shoppingItem.user_id}
+              category_name={shoppingItem.category_name}
+            />
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 }
 
 export default RecentFinds;
